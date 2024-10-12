42515ae7fdbe0b0a969dca36c198bb07a76eba5f
Harsh <harshpareek91@gmail.com>
2024-10-12 11:54:32 +0530

    fix: rm workspace

--- app/workspaces/route.info.ts
+++
@@ -1,9 +0,0 @@
-import createRoute, { EmptyRouteParams } from "../../confug/route.config";
-
-const WorkspacesPageRoute = createRoute({
-  name: "/workspaces",
-  paramsSchema: EmptyRouteParams,
-  fn: () => "/workspaces",
-});
-
-export default WorkspacesPageRoute;
